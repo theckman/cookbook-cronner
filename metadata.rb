26495848ceb1e1f999a7ff8df7edc5be83b133c6
Tim Heckman <t@heckman.io>
2017-01-02 19:02:35 -0800

    add files to prepare for supermarket uploading
    This adds a `metadata.rb` file and `chefignore` file. Both of which are
    important when pushing cookbooks to the Chef Supermarket.

---
+++ metadata.rb
@@ -0,0 +1,26 @@
+# Copyright 2017 Tim Heckman <t@heckman.io>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+name 'cronner'
+version '0.1.0'
+license 'Apache 2.0'
+
+description 'Installs/Configures cronner'
+long_description 'Installs/Configures cronner'
+
+maintainer 'Tim Heckman'
+maintainer_email 't@heckman.io'
+
+issues_url 'https://github.com/theckman/cronner/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/theckman/cronner' if respond_to?(:source_url)
