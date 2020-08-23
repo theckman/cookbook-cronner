ac99e560c3ab23a5c68c02f38187ad49b3b9b844
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-08-22 23:05:37 -0400

    tests: Add testing on Ubuntu 20.04

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -19,8 +19,8 @@
 require 'spec_helper'
 
 describe 'cronner::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
-    let(:chef_run) { ChefSpec::ServerRunner.new.converge described_recipe }
+  context 'When all attributes are default, on Ubuntu 20.04' do
+    platform 'ubuntu', '20.04'
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
